ecd304db9cae901398ffc83b7c5adfcd17bbd2ee
Cai YiChen <henry050628@163.com>
2026-04-02 20:57:01 +0800

    finish shade function in material.hpp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc != 3) {
-        cout << "Usage: ./bin/PA1 <input scene file> <output bmp file>" << endl;
+        std::cout << "Usage: ./bin/PA1 <input scene file> <output bmp file>" << endl;
         return 1;
     }
     string inputFile = argv[1];
@@ -29,11 +29,49 @@ int main(int argc, char *argv[]) {
 
     // TODO: Main RayCasting Logic
     // First, parse the scene using SceneParser.
-    // Then loop over each pixel in the image, shooting a ray
     // through that pixel and finding its intersection with
     // the scene.  Write the color at the intersection to that
     // pixel in your output image.
-    cout << "Hello! Computer Graphics!" << endl;
+    SceneParser sceneParser = SceneParser(inputFile.c_str());
+    // Then loop over each pixel in the image, shooting a ray
+    Camera* camera = sceneParser.getCamera();
+    int width = camera->getWidth();
+    int height = camera->getHeight();
+    Image image(width,height);
+
+    for(int x=0; x < width;x++){
+        for(int y=0;y < height;y++){
+            //shooting a ray
+            Ray ray = camera->generateRay(Vector2f(x,y));
+            Group* basegroup = sceneParser.getGroup();
+            Hit hit;
+            //判断ray和basegroup是否有交点。并且将结果存储在hit中
+            bool isIntersect = basegroup->intersect(ray,hit,0);
+            if(isIntersect){
+                //说明有交点
+                //累加来自所有光源的光强影响
+                Vector3f finalColor = Vector3f::ZERO;
+                for(int li = 0; li < sceneParser.getNumLights();li++){
+                    Light* light = sceneParser.getLight(li);
+                    //获得光照以及光照
+                    Vector3f L,lightColor;
+                    light->getIllumination(ray.pointAtParameter(hit.getT()),L,lightColor);
+                    finalColor += hit.getMaterial()->Shade(ray,hit,L,lightColor);
+                }
+                image.SetPixel(x,y,finalColor);
+                
+            }else{
+                //说明不存在交点
+                image.SetPixel(x,y,sceneParser.getBackgroundColor());
+            }
+
+        }
+
+    }
+
+    image.SaveImage(outputFile.c_str());
+    
+    std::cout << "Hello! Computer Graphics!" << endl;
     return 0;
 }
 
